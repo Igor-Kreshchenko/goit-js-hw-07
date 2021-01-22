ac8107b610af1b567dd0cc558ca2e02317d24aaf
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-01-22 18:35:08 +0200

    task 1, 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 const listRef = document.querySelector('#categories');
-const categories = document.querySelectorAll('.item');
+const categoriesRef = document.querySelectorAll('.item');
 
 const getCategoriesNumber = function (listRef) {
   const categoriesNumber = listRef.children.length;
@@ -9,16 +9,13 @@ const getCategoriesNumber = function (listRef) {
 
 getCategoriesNumber(listRef);
 
-const showCategoriesTitles = function (categories) {
+const showDescrOfCategories = function (categories) {
   categories.forEach(category => {
     console.log(`Категория: ${category.firstElementChild.textContent}`);
     console.log(
-      `Количество элементов: ${category.children[1].children.length}`,
+      `Количество элементов: ${category.lastElementChild.children.length}`,
     );
   });
 };
 
-showCategoriesTitles(categories);
-
-// Без функций
-const listRef = document.querySelector('#categories');
+showDescrOfCategories(categoriesRef);
