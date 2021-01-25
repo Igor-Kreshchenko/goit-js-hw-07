e630d3b207bd65522655fa138ad538c8980eb46a
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-01-25 13:14:08 +0200

    tasks 6-7

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,14 @@
+const inputRef = document.querySelector('#validation-input');
+const dataLengthValue = inputRef.getAttribute('data-length');
 
+const validationCheck = () => {
+  if (inputRef.value.length !== Number(dataLengthValue)) {
+    inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
+  } else {
+    inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  }
+};
+
+inputRef.addEventListener('blur', validationCheck);
